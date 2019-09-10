3521b546dd25e793da1b7b73aae67d5ef7f11673
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-10 21:23:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA"
-    puts "I LOVE YOU TOO PUMPKIN"
+     "I LOVE YOU TOO PUMPKIN"
     
   else
     puts "HUH?! SPEAK UP, SONNY!"
